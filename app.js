245fbe1aabe7be856aa7f1ef5e9ebaa3558620b9
Anna Crabb <annacrabb92@gmail.com>
2024-05-20 20:20:01 -0400

    new updates

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 var shopOne = [
-    
-    {
-      title: 'Yellow Seahorse',
-      image: 'https://i1.pickpik.com/photos/455/349/247/seahorse-sea-horse-yellow-marine-preview.jpg',
-      price: '10 per month',
-      description: 'words'
-    },
+  
 
-    {
-      title: 'Chromodoris Nudibranch',
-      image: 'https://i2.pickpik.com/photos/75/853/134/nudibranch-chromodoris-slug-sea-slug-preview.jpg',
-      price: '10 per month',
-      description: 'words'
-    },
+    // {
+    //   title: 'Chromodoris Nudibranch',
+    //   image: 'https://i2.pickpik.com/photos/75/853/134/nudibranch-chromodoris-slug-sea-slug-preview.jpg',
+    //   price: '10 per month',
+    //   description: 'words'
+    // },
 
     {
       title: 'Spanish Shawl Nudibranch',
@@ -28,6 +22,44 @@ var shopOne = [
       description: 'words'
     },
 
+    {
+      title: 'Cleaner Shrimp',
+      image: 'assets/cleaner.shrimp.curacao.jack.png',
+      price: '10 per month',
+      description: 'words'
+    },
+
+    {
+      title: 'Harlequin Shrimp',
+      image: 'assets/harlequin.shrimp.costa.rica.jack.png',
+      price: '10 per month',
+      description: 'words'
+    },
+    {
+      title: 'Hermit Crab',
+      image: 'assets/hermit.crab.cozumel.jack.png',
+      price: '10 per month',
+      description: 'words'
+    },
+    {
+      title: 'Starfish',
+      image: 'assets/seastar.florida.jack.png',
+      price: '10 per month',
+      description: 'words'
+    },
+    {
+      title: 'Trumpetfish',
+      image: 'assets/trumpetfish.florida.jack.png',
+      price: '10 per month',
+      description: 'words'
+    },
+
+    {
+      title: 'Yellow Seahorse',
+      image: 'https://i1.pickpik.com/photos/455/349/247/seahorse-sea-horse-yellow-marine-preview.jpg',
+      price: '10 per month',
+      description: 'words'
+    },
     ]
     
     var postHTML = " "
@@ -44,42 +76,82 @@ var shopOne = [
 
 
 var shopTwo = [
+    // {
+    //   title: 'Longhorn Cowfish',
+    //   image: 'https://i2.pickpik.com/photos/925/46/391/longhorn-cowfish-reef-fish-horned-boxfish-sea-preview.jpg',
+    //   price: '50 per month',
+    //   description: 'words'
+    // },
+    
+    // {
+    //   title: 'Brown Tortoise',
+    //   image: 'https://i1.pickpik.com/photos/105/153/284/turtle-underwater-marine-fish-preview.jpg',
+    //   price: '50 per month',
+    //   description: 'words'
+    // },
+
+    // {
+    //   title: 'Trumpet Fish',
+    //   image: 'https://live.staticflickr.com/65535/51939573062_1b34f537e9_b.jpg',
+    //   price: '50 per month',
+    //   description: 'words'
+    // },
+
+    // {
+    //   title: 'Cuttlefish',
+    //   image: 'https://live.staticflickr.com/3863/14412947916_d11dc50ca8_b.jpg',
+    //   price: '50 per month',
+    //   description: 'words'
+    // // },
+
+    // {
+    //   title: 'Hawaiian Spiny Lobster',
+    //   image: 'https://live.staticflickr.com/2908/13975389495_5b17616147_b.jpg',
+    //   price: '50 per month',
+    //   description: 'words'
+    // },
+    
     {
-      title: 'Longhorn Cowfish',
-      image: 'https://i2.pickpik.com/photos/925/46/391/longhorn-cowfish-reef-fish-horned-boxfish-sea-preview.jpg',
+      title: 'Blue Toadfish',
+      image: 'assets/bluetoadfish.cozumel.jack.png',
+      price: '50 per month',
+      description: 'words'
+    },
+
+    {
+      title: 'Damselfish',
+      image: 'assets/damselfish.cozumel.jack.png',
+      price: '50 per month',
+      description: 'words'
+    },
+
+    {
+      title: 'Pufferfish',
+      image: 'assets/puffer.fish.florida.jack.png',
+      price: '50 per month',
+      description: 'words'
+    },
+
+    {
+      title: 'Queen Triggerfish',
+      image: 'assets/queen.triggerfish.cozumel.jack.png',
+      price: '50 per month',
+      description: 'words'
+    },
+
+    {
+      title: 'Sea Turtle',
+      image: 'assets/sea.turtle.florida.jack.png',
       price: '50 per month',
       description: 'words'
     },
     
     {
-      title: 'Brown Tortoise',
-      image: 'https://i1.pickpik.com/photos/105/153/284/turtle-underwater-marine-fish-preview.jpg',
+      title: 'Squirrelfish',
+      image: 'assets/squirrelfish.cozumel.jack.png',
       price: '50 per month',
       description: 'words'
     },
-
-    {
-      title: 'Trumpet Fish',
-      image: 'https://live.staticflickr.com/65535/51939573062_1b34f537e9_b.jpg',
-      price: '50 per month',
-      description: 'words'
-    },
-
-    {
-      title: 'Cuttlefish',
-      image: 'https://live.staticflickr.com/3863/14412947916_d11dc50ca8_b.jpg',
-      price: '50 per month',
-      description: 'words'
-    },
-
-    {
-      title: 'Hawaiian Spiny Lobster',
-      image: 'https://live.staticflickr.com/2908/13975389495_5b17616147_b.jpg',
-      price: '50 per month',
-      description: 'words'
-    },
-    
-
 
     ]
     
@@ -107,15 +179,29 @@ var shopThree = [
     },
     
     {
-      title: 'Nurse Shark',
-      image: 'https://live.staticflickr.com/3894/14319397510_dcd8dc6ab8_b.jpg',
+      title: 'Sealion',
+      image: 'assets/sealion.jack.png',
       price: '100 per month',
       description: 'words'
     },
-    
+
     {
-      title: 'Spotted Eagle Ray',
-      image: 'https://live.staticflickr.com/2866/8758452682_6320c05934_b.jpg',
+      title: 'Hammerhead Shark',
+      image: 'assets/hammerhead.florida.jack.png',
+      price: '100 per month',
+      description: 'words'
+    },
+
+    {
+      title: 'Nurse Shark',
+      image: 'assets/nurseshark.cozumel.jack.png',
+      price: '100 per month',
+      description: 'words'
+    },
+
+    {
+      title: 'Spotted Eagle Sting Ray',
+      image: 'assets/ray.cozumel.jack.png',
       price: '100 per month',
       description: 'words'
     },
